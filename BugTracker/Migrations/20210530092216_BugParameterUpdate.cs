a107826e4330ced7c2e953cc52b3a9f7f587daa6
William Olive JR <wolive002@gmail.com>
2021-05-30 05:31:08 -0400

    Update the bug model with date and user name

---
+++ BugTracker/Migrations/20210530092216_BugParameterUpdate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTracker.Migrations
+{
+    public partial class BugParameterUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Bug",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Bug",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Bug");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Bug");
+        }
+    }
+}
