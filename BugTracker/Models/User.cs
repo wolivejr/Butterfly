761887a48e6c8cdefc71d12f6acdb9415ebff44b
William Olive JR <wolive002@gmail.com>
2021-05-31 06:40:20 -0400

    Added database, tweaked forms

--- BugTracker/Models/User.cs
+++ BugTracker/Models/User.cs
@@ -7,8 +7,10 @@ namespace BugTracker.Models
 {
     public class User
     {
+
         public int ID { get; set; }
         public string Username { get; set; }
         public string Role { get; set; }
+    
     }
 }
